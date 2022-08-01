eacc15d916daeb84cb68b5acf8c2e286d5dbd278
TECHRITOMA Inc <ajuatah@gmail.com>
2022-08-01 09:34:31 +0100

    Add project files.

---
+++ WebAPI/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI.Controllers
+{
+    public class UserController : ApiController
+    {
+        [HttpGet]
+        public string Greet(string name)
+        {
+            return $"Welcome {name}";
+        }
+    }
+}
